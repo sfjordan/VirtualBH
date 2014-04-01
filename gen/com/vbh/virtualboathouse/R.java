1737b6f8286eee97de5949a04fd56e6c3cf2afa8
Sam Jordan <samuelfjordan@gmail.com>
2014-04-01 14:30:40 -0400

    add splashactivity

--- gen/com/vbh/virtualboathouse/R.java
+++ gen/com/vbh/virtualboathouse/R.java
@@ -27,17 +27,17 @@ public final class R {
     public static final class id {
         public static final int action_settings=0x7f080010;
         public static final int clear_button=0x7f080005;
-        public static final int clear_button2=0x7f08000b;
+        public static final int clear_button2=0x7f08000a;
         public static final int container=0x7f080000;
         public static final int enter_num_field=0x7f08000d;
         public static final int enter_num_text=0x7f08000e;
         public static final int field=0x7f080002;
-        public static final int field2=0x7f080008;
+        public static final int field2=0x7f080009;
         public static final int go_button=0x7f08000f;
         public static final int millichrono=0x7f080004;
-        public static final int millichrono2=0x7f08000a;
+        public static final int millichrono2=0x7f080008;
         public static final int start_button=0x7f080003;
-        public static final int start_button2=0x7f080009;
+        public static final int start_button2=0x7f08000b;
         public static final int stop_button=0x7f080006;
         public static final int stop_button2=0x7f08000c;
         public static final int timer2_container=0x7f080007;
@@ -46,12 +46,15 @@ public final class R {
     public static final class layout {
         public static final int activity_display_timers=0x7f030000;
         public static final int activity_main=0x7f030001;
-        public static final int fragment_display_timers=0x7f030002;
-        public static final int fragment_main=0x7f030003;
+        public static final int activity_splashscreenactivity=0x7f030002;
+        public static final int fragment_display_timers=0x7f030003;
+        public static final int fragment_main=0x7f030004;
+        public static final int fragment_splashscreenactivity=0x7f030005;
     }
     public static final class menu {
         public static final int display_timers=0x7f070000;
         public static final int main=0x7f070001;
+        public static final int splashscreenactivity=0x7f070002;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
@@ -65,9 +68,11 @@ public final class R {
         public static final int edit_message=0x7f050003;
         public static final int enter_num_timers=0x7f05000a;
         public static final int hello_text=0x7f050006;
+        public static final int hello_world=0x7f05000f;
         public static final int timer_name_text=0x7f05000d;
         public static final int title_activity_display_timers=0x7f050002;
         public static final int title_activity_main=0x7f050005;
+        public static final int title_activity_splashscreenactivity=0x7f05000e;
     }
     public static final class style {
         /** 
