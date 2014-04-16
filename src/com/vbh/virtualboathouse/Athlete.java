e27a396f79196590951c9690cfecba4a082821e9
Matthew Drabick <mdrabick@princeton.edu>
2014-04-16 15:09:15 -0400

    started new classes Athlete and Roster

---
+++ src/com/vbh/virtualboathouse/Athlete.java
@@ -0,0 +1,16 @@
+package com.vbh.virtualboathouse;
+
+import java.io.Serializable;
+
+public class Athlete implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7530572379516212709L;
+
+	public Athlete(AthleteModel am) {
+		
+	}
+	
+}
